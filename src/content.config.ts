237c08fd8c4d9066635f23e098541c42ca79e024
Jakub Kontra <kontrajakub2@gmail.com>
2025-09-21 15:47:51 +0000

    fixed roouting, adn shema to work

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,9 @@
 // 1. Import utilities from `astro:content`
 import { defineCollection, z } from "astro:content";
 
-// 2. Import loader(s)
-import { glob } from "astro/loaders";
-
-// 3. Define your collection(s)
+// 2. Define your collection(s)
 
 const blog = defineCollection({
-  // `loader` can accept an array of multiple patterns as well as string patterns
-  // Load all markdown files in the space-probes directory, except for those that start with "voyager-"
-  loader: glob({
-    pattern: ["*.md", "!voyager-*"],
-    base: "src/content/blog",
-  }),
     schema: z.object({
     title: z.string(),
     date: z.date(),
